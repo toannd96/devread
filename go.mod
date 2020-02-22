7dd33024490db16e1222debc86c8214627755c10
dactoankmapydev <nguyendactoankma@gmail.com>
2020-02-23 00:12:18 +0700

    migrations db

--- go.mod
+++ go.mod
@@ -7,5 +7,7 @@ require (
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0
 	github.com/lib/pq v1.3.0
+	github.com/mattn/go-oci8 v0.0.4 // indirect
+	github.com/rubenv/sql-migrate v0.0.0-20200212082348-64f95ea68aa3 // indirect
 	golang.org/x/crypto v0.0.0-20200214034016-1d94cc7ab1c6 // indirect
 )
---
+++ handler/migrations/1_init.sql
@@ -0,0 +1,39 @@
+-- +migrate Up
+CREATE TABLE "users" (
+  "user_id" text PRIMARY KEY,
+  "full_name" text,
+  "email" text UNIQUE,
+  "password" text,
+  "role" text,
+  "created_at" TIMESTAMPTZ NOT NULL,
+  "updated_at" TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE "posts" (
+  "name" text PRIMARY KEY,
+  "url" text,
+  "category" text,
+  "view" text,
+  "share" text,
+  "comment" text,
+  "build_by" text,
+  "time_read" text,
+  "created_at" TIMESTAMPTZ NOT NULL,
+);
+
+CREATE TABLE "bookmarks" (
+  "bid" text PRIMARY KEY,
+  "user_id" text,
+  "post_name" text,
+  "created_at" TIMESTAMPTZ NOT NULL,
+  "updated_at" TIMESTAMPTZ NOT NULL,
+  unique (user_id, post_name)
+);
+
+ALTER TABLE "bookmarks" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("user_id");
+ALTER TABLE "bookmarks" ADD FOREIGN KEY ("post_name") REFERENCES "posts" ("name");
+
+-- +migrate Down
+DROP TABLE users;
+DROP TABLE posts;
+DROP TABLE bookmarks;
