1ccf69b11a2ba59c1b39898fb90a65ca90b9e923
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-08-30 01:12:29 +0700

    Update user_handler.go

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -102,7 +102,7 @@ func (u *UserHandler) SignUp(c echo.Context) error {
 		})
 	}
 
-	link := "http://localhost:3000" + "/user/verify?token=" + token
+	link := "https://devread.herokuapp.com" + "/user/verify?token=" + token
 
 	from := os.Getenv("FROM")
 	password := os.Getenv("PASSWORD")
@@ -191,7 +191,7 @@ func (u *UserHandler) ForgotPassword(c echo.Context) error {
 		})
 	}
 
-	link := "http://localhost:3000" + "/user/password/reset?token=" + token
+	link := "https://devread.herokuapp.com" + "/user/password/reset?token=" + token
 
 	from := os.Getenv("FROM")
 	password := os.Getenv("PASSWORD")
